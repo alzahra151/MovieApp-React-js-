dbad4b4d2f3d56601ae750908e5063b12122ae20
Alzahraa Desoky <33639931+alzahra151@users.noreply.github.com>
2023-01-29 01:38:32 +0200

    chang background

--- src/App.js
+++ src/App.js
@@ -5,50 +5,49 @@ import AboutMe from './componants/AboutMe';
 import Skills from './componants/Skills';
 import Portfolio from './componants/Portfolio';
 import Footer from './componants/Footer';
- import Portcard from './componants/Portcard';
-import Register from './ToDo&Regester/Register';
+import Portcard from './componants/Portcard';
+import Register from './Regester/Register';
 import Movie from './apicomponant/Movie';
 import Todo from './todoApp/Todo';
-import NavBar  from './apicomponant/NavBar';
-import {LanguagProvider} from './context/context'
+import NavBar from './apicomponant/NavBar';
+import { LanguagProvider } from './context/context'
 import {
   BrowserRouter as Router,
-  Switch ,
+  Switch,
   Route
 }
-from 'react-router-dom'
+  from 'react-router-dom'
 import MovieDetails from './apicomponant/MovieDetails';
 import Favourite from './apicomponant/Favourite';
 import { useState } from 'react';
 
 
 function App() {
-  const [languag ,setLanguag]=useState("en")
+  const [languag, setLanguag] = useState("en")
   return (
     <div className="App">
-   
-    {/* <Header></Header>
+
+      {/* <Header></Header>
     <AboutMe/>
     <Skills/>
     <Portfolio />
-    <Footer/> */}
-{/* /////////////// */}
-{/* 
-<Register/> */}
-{/* <Todo/> */}
-<Router>
-<LanguagProvider value={{languag ,setLanguag}}>
-<NavBar/>
-{/* <Movie/> */}
-  <Switch>
-  <Route exact path='/' component={Movie}/>
-    <Route exact path='/register' component={Register}/>
-    <Route exact path='/movies' component={Movie}/>
-    <Route exact path='/favourite' component={Favourite}/>
-    <Route exact path='/details/:id' component={MovieDetails}/>
-  </Switch>
-  </LanguagProvider>
-</Router>
+    <Footer/> 
+     
+   <Register/> */} 
+      
+      <Router>
+        <LanguagProvider value={{ languag, setLanguag }}>
+          <NavBar />
+          {/* <Movie/> */}
+          <Switch>
+            <Route exact path='/' component={Movie} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/movies' component={Movie} />
+            <Route exact path='/favourite' component={Favourite} />
+            <Route exact path='/details/:id' component={MovieDetails} />
+          </Switch>
+        </LanguagProvider>
+      </Router>
     </div>
 
   );
--- src/apicomponant/Movie.js
+++ src/apicomponant/Movie.js
@@ -1,26 +1,26 @@
-import React, { useEffect ,useState ,useContext} from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
 import axiosInst from '../axiosConfig/axiosInstance'
-import { useDispatch ,useSelector } from 'react-redux'
-import { displayArticls, increaseCount ,increaseFav, nextArticls, prevArticls} from './../store/Action'
-import {Movies} from './Movies.css' 
+import { useDispatch, useSelector } from 'react-redux'
+import { displayArticls, increaseCount, increaseFav, nextArticls, prevArticls } from './../store/Action'
+import { Movies } from './Movies.css'
 import { languagContext } from '../context/context'
 
 
 export default function Movie() {
-//  let [movies ,setMovies]=useState([])
-let movies=useSelector((state)=>state.movies)
-//  const params=useParams()
+  //  let [movies ,setMovies]=useState([])
+  let movies = useSelector((state) => state.movies)
+  //  const params=useParams()
 
- let FavCounter =useSelector((state)=>state.counter)
- var favArr=useSelector((state)=>state.favourites)
-//  var [favArr, setFav]=useState([])
- const dispatch =useDispatch()
- const {languag ,setLanguag}=useContext(languagContext)
-// let [counter, setCounter]=useState(1)
-    useEffect(()=>{
+  let FavCounter = useSelector((state) => state.counter)
+  var favArr = useSelector((state) => state.favourites)
+  //  var [favArr, setFav]=useState([])
+  const dispatch = useDispatch()
+  const { languag, setLanguag } = useContext(languagContext)
+  // let [counter, setCounter]=useState(1)
+  useEffect(() => {
     //    axiosInst.get(`/3/movie/popular?${params.apikey}`).then((res)=>{
     //         console.log(res.data.results)
     //        setMovies(res.data.results)
@@ -28,76 +28,74 @@ let movies=useSelector((state)=>state.movies)
     //         console.log(err)
     //     })
     dispatch(displayArticls())
-    },[])
-  function  next(){
-        
-        // setCounter(++counter)
-        // axiosInst.get(`/3/movie/popular?${params.apikey}`,{params:{page:counter}}).then((res)=>{
-        //     console.log(res.data.results)
-        // //  movies= res.data.results
-        // }).catch((err)=>{
-        //     console.log(err)
-        // })
-          dispatch(nextArticls())
-         dispatch(displayArticls())
-        
+  }, [])
+  function next() {
+
+    // setCounter(++counter)
+    // axiosInst.get(`/3/movie/popular?${params.apikey}`,{params:{page:counter}}).then((res)=>{
+    //     console.log(res.data.results)
+    // //  movies= res.data.results
+    // }).catch((err)=>{
+    //     console.log(err)
+    // })
+    //next button using Redux
+    dispatch(nextArticls())
+    dispatch(displayArticls())
+
+  }
+  function prev() {
+    // setCounter(--counter)
+    // axiosInst.get(`/3/movie/popular?${params.apikey}`,{params:{page:counter}}).then((res)=>{
+    //     console.log(res.data.results)
+    //    movies=res.data.results
+    // }).catch((err)=>{
+    //     console.log(err)
+    // })
+    // prev button using redux
+    dispatch(prevArticls())
+    dispatch(displayArticls())
+
+  }
+  const addFav = (e) => {
+    let exitemovie = favArr.find((movie) => {
+      return movie.id == e.id
+    })
+
+    if (!exitemovie) {
+      favArr.push(e)
     }
-    function  prev(){
-       
-        // setCounter(--counter)
-        // axiosInst.get(`/3/movie/popular?${params.apikey}`,{params:{page:counter}}).then((res)=>{
-        //     console.log(res.data.results)
-        //    movies=res.data.results
-        // }).catch((err)=>{
-        //     console.log(err)
-        // })
-        dispatch(prevArticls())
-           dispatch(displayArticls())
-        
-    }
-    const addFav=(e)=>{
-       let exitemovie=  favArr.find((movie)=>{
-                return movie.id==e.id
-         })
-      
-        if(!exitemovie){
-            favArr.push(e)
-        }
-       
-        dispatch(increaseFav(favArr))
-        console.log(favArr)
-        
-        dispatch(increaseCount(favArr.length))
-    }
+
+    dispatch(increaseFav(favArr))
+    console.log(favArr)
+
+    dispatch(increaseCount(favArr.length))
+  }
   return (
-    <div>
-     {/* <button className='btn btn-primary' onClick={()=>{setLanguag((languag=="en")?"Ar":"en")}}>
-        language
-        </button> */}
-        {/* <span>{languag}</span> */}
-    <div className='container d-flex flex-wrap'>
-        {movies.map((movie)=>{
-            return (
-                <div className="card w-25  m-4" onClick={()=>{addFav(movie)}} key={movie.id} >
-                <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} />
-                <div className="card-body">
-                  <h5 className="card-title">{movie.title}</h5>
-            <button class="border-0 bg-light"> <i class={`bi ${(favArr.find((m)=>m.id==movie.id))?"bi-heart-fill text-danger":"bi-heart text-danger"} `}></i> </button>
-            
-               <div>
-               <Link  to={`/details/${movie.id}`} >
-                  <a href="#" className="btn details">Go Details</a>
-                  </Link>
-               </div>
+    <div className="moviesContainer" >
+      <div className='container d-flex flex-wrap'>
+        {movies.map((movie) => {
+          return (
+            <div className='card w-25  m-4'>
+            <div className="card" onClick={() => { addFav(movie) }} key={movie.id} >
+              <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} />
+              <div className="card-body">
+                <h5 className="card-title">{movie.title}</h5>
+                <button class="border-0 bg-light"> <i class={`bi ${(favArr.find((m) => m.id == movie.id)) ? "bi-heart-fill text-danger" : "bi-heart text-danger"} `}></i> </button>
+                <div>
+                </div>
               </div>
-              </div>
-            )
+            </div>
+            <Link to={`/details/${movie.id}`} >
+                    <a href="#" className="btn details">Go Details</a>
+            </Link>
+            </div>
+          )
         })}
-       
-    </div>
-   
-   <button onClick={()=>{next()}} className='btn btn-success m-5 p-3'> next</button>
-        <button onClick={()=>{prev()}} className='btn btn-success m-5 p-3'>prev</button>
+
+      </div>
+
+      <button onClick={() => { next() }} className='btn btn-success m-5 p-3'> next</button>
+      <button onClick={() => { prev() }} className='btn btn-success m-5 p-3'>prev</button>
     </div>
   )
 }
